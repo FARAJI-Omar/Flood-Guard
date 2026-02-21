c919fcf0dfd9a62701a6ab7ba3087ff050cfeff8
F99 <farajiomar99@gmail.com>
2026-02-21 14:10:00 +0100

    core: Add unit test for FloodGuard application context

---
+++ src/test/java/com/floodguard/FloodGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floodguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloodGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
